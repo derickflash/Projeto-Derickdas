25d4384215bbd722888d0364a63defbdf48f2545
derickflash <133686479+derickflash@users.noreply.github.com>
2024-11-19 08:28:03 -0300

    Add files via upload

---
+++ Projeto-Derickdas-main/Projeto-Derickdas-4f99d3b58f6c2072bc38044385cbf53f0e17d92d/continuação flashcard/Projeto-Derickdas-main/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Neymar ',
+    'Ele tem quantos champions?',
+    'Por enquanto 1'
+)
+
+criaCartao(
+    'Juranda',
+    'Qual a pessoa mais rica do brasil?',
+    'Feu Moreira Dos Santos',
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
